39c15a9c6da52f43ed93b104602670926dfe88c2
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-19 15:59:38 +0330

    ✨ feat(contact): better responsive for mobile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -19,17 +19,17 @@ const ImageSource =
 export default function About() {
   return (
     <section className="flex h-full items-start justify-center  md:h-screen ">
-      <div className="grid h-full w-full grid-cols-1  p-3 max-md:gap-5 md:w-[80%] md:grid-cols-2">
-        <div className="flex flex-col items-center justify-center space-y-3 ">
+      <div className="grid  h-full w-full grid-cols-1  p-3 max-md:gap-5 md:w-[80%] md:grid-cols-2">
+        <div className="flex flex-col items-center justify-center max-md:pt-14">
           <h2 className="text-3xl font-medium text-blue-600 md:text-left">
             Get In Touch
           </h2>
-          <p className="text-blue-900 md:text-left">
+          <p className="py-4 text-blue-900 md:text-left">
             We are here for you! How can we help?
           </p>
           <ContactForm />
         </div>
-        <div className="flex  flex-col items-center justify-start gap-5">
+        <div className="flex flex-col items-center justify-start">
           <Image
             src={ImageSource}
             alt="About Us Image"
