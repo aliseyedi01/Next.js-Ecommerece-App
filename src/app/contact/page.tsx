36862147fc8818c8e2eec91d14c5c586b2c67ac4
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-19 21:47:31 +0330

    ✨ feat(contact): change order show section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,11 +11,20 @@ export const metadata: Metadata = {
 const ImageSource =
   "https://img.freepik.com/free-vector/customer-service-operators-with-headsets-computers-consulting-clients-24-7-call-center-handling-call-system-virtual-call-center-concept-illustration_335657-2329.jpg?t=st=1710852309~exp=1710855909~hmac=e1690b510ba8a6ce6a8dcbbccc92f93cef242ec72cba1bf17c86af09e8dd359f&w=996";
 
-export default function About() {
+export default function Contact() {
   return (
     <section className="flex h-full items-start justify-center  md:h-screen ">
-      <div className="grid  h-full w-full grid-cols-1  p-3 max-md:gap-5 md:w-[85%] md:grid-cols-2">
-        <div className="flex flex-col items-center justify-start">
+      <div className="grid h-full w-full grid-cols-1  p-3 max-md:gap-5 md:w-[85%] md:grid-cols-2">
+        <div className="flex flex-col items-center justify-start pt-16 md:pt-8">
+          <h2 className="text-3xl font-medium text-blue-600 md:text-left">
+            Get In Touch
+          </h2>
+          <p className="py-4 text-blue-900 md:text-left">
+            We are here for you! How can we help?
+          </p>
+          <ContactForm />
+        </div>
+        <div className="flex flex-col items-center justify-start max-md:py-9">
           <Image
             src={ImageSource}
             alt="About Us Image"
@@ -24,15 +33,6 @@ export default function About() {
             className="rounded-lg object-cover"
           />
           <SocialMedia />
-        </div>
-        <div className="flex flex-col items-center justify-start py-14">
-          <h2 className="text-3xl font-medium text-blue-600 md:text-left">
-            Get In Touch
-          </h2>
-          <p className="py-4 text-blue-900 md:text-left">
-            We are here for you! How can we help?
-          </p>
-          <ContactForm />
         </div>
       </div>
     </section>
