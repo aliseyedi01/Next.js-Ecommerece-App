07edcfb024ccfd474d9d42ce4a60eb766a06c68e
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-22 20:10:49 +0330

    ✨ feat(newsletter): update for responsive

--- src/components/home/NewsLetter.tsx
+++ src/components/home/NewsLetter.tsx
@@ -9,21 +9,21 @@ const Newsletter = () => {
   return (
     <section className="py-14">
       <div className="mx-auto max-w-7xl px-5 sm:px-6">
-        <div className="rid-rows-2 grid grid-flow-row md:grid-flow-col md:grid-cols-2 md:gap-5">
+        <div className="rid-rows-2 grid grid-flow-row place-content-center gap-5 md:grid-flow-col md:grid-cols-2 ">
           <Image
             src="/images/pages/newsletter.svg"
             alt="About Us Image"
             width={450}
             height={450}
-            className="order-2 object-cover md:order-1 md:row-span-2"
+            className="order-2 md:order-1 md:row-span-2"
           />
 
-          <div className="order-1 flex flex-col items-start justify-end pl-14">
+          <div className="order-1 flex flex-col items-start justify-end pl-5 text-primary md:pl-14">
             <p className="text-xl font-medium ">Subscribe to</p>
             <p className="text-4xl font-semibold leading-none">
               our newsletter
             </p>
-            <p className="w-[25rem] py-3 text-base font-medium text-gray-500">
+            <p className="w-full py-3   text-base font-medium text-primary/70 md:w-[25rem]">
               Get 20% off on your first order just by subscribing to our
               newsletter
             </p>
@@ -31,11 +31,11 @@ const Newsletter = () => {
 
           <div className="order-3 w-full">
             <div className="mx-auto flex w-full max-w-md flex-wrap justify-center py-3">
-              <div className="relative w-4/5 ">
+              <div className="relative w-full md:w-4/5 ">
                 <Input
                   type="email"
                   placeholder="user@gmail.com"
-                  className=" h-auto w-full rounded-full py-3 pl-12 placeholder:font-medium placeholder:text-slate-400"
+                  className="h-auto w-full rounded-full py-3 pl-12 placeholder:font-medium placeholder:text-blue-800/50"
                 />
                 <Button
                   className="absolute bottom-0 right-0 h-full rounded-r-full"
@@ -44,9 +44,9 @@ const Newsletter = () => {
                   Subscribe
                 </Button>
               </div>
-              {/* <p className="mt-4  text-sm  text-gray-500">
+              <p className="mt-4  text-sm  text-blue-400">
                 We promise not to span you !
-              </p> */}
+              </p>
             </div>
           </div>
         </div>
