bf4289fc41df2c88182951ae17a633aa6349bb7b
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-27 09:18:59 +0330

    ✨ feat(pwa): add runtime caching configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 import withPWA from "next-pwa";
+import runtimeCaching from "next-pwa/cache.js";
 
 const makePWA = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  sw: "service-worker.js",
+  runtimeCaching,
 });
 
 const nextConfig = {
