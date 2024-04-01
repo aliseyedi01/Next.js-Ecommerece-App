15b95737250e5c036018a36e1ce5b7259914a12c
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-04-01 04:45:12 +0330

    ✨ feat(profile): add ring to image for selected

--- src/app/(profile)/profile/edit/_components/edit-image-profile.tsx
+++ src/app/(profile)/profile/edit/_components/edit-image-profile.tsx
@@ -1,4 +1,11 @@
-import { Button, Separator } from "@components/ui";
+"use client";
+import {
+  Button,
+  Label,
+  RadioGroup,
+  RadioGroupItem,
+  Separator,
+} from "@components/ui";
 import {
   avatar1,
   avatar2,
@@ -8,20 +15,27 @@ import {
   avatar6,
 } from "@constants/images";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const avatarOptions = [
-  { src: avatar1, alt: "Avatar 1" },
-  { src: avatar2, alt: "Avatar 2" },
-  { src: avatar3, alt: "Avatar 3" },
-  { src: avatar4, alt: "Avatar 4" },
-  { src: avatar5, alt: "Avatar 5" },
-  { src: avatar6, alt: "Avatar 6" },
+  { src: avatar1, alt: "Avatar-1" },
+  { src: avatar2, alt: "Avatar-2" },
+  { src: avatar3, alt: "Avatar-3" },
+  { src: avatar4, alt: "Avatar-4" },
+  { src: avatar5, alt: "Avatar-5" },
+  { src: avatar6, alt: "Avatar-6" },
 ];
 
 export default function EditPhotoProfile() {
+  const [selectedAvatar, setSelectedAvatar] = useState();
+
+  useEffect(() => {
+    console.log("selectedAvatar changed:", selectedAvatar);
+  }, [selectedAvatar]);
+
   return (
     <div className="flex h-fit w-1/2 flex-col overflow-hidden rounded-lg bg-slate-100">
+      {/* Photo */}
       <div className="relative h-20  w-full bg-gradient-to-r from-blue-300 to-yellow-300">
         <Image
           src={avatar1}
@@ -31,10 +45,10 @@ export default function EditPhotoProfile() {
           className="border-1 absolute -bottom-7 left-6 size-20 rounded-3xl border-white"
         ></Image>
       </div>
-
+      {/* Photo Information & Selected */}
       <div className="flex  flex-row items-center justify-start space-x-4 ">
+        {/* Photo Profile */}
         <div className="mt-8 space-y-4 p-3 pr-2">
-          {/* Photo Profile */}
           <div className="text-sm text-slate-500">
             <h3 className="mb-2 font-semibold text-blue-950">
               Your Profile Photo
@@ -50,13 +64,22 @@ export default function EditPhotoProfile() {
             </Button>
           </div>
         </div>
-        <Separator orientation="vertical" className="h-40  bg-slate-500" />
+        <Separator orientation="vertical" className="h-40 bg-slate-500" />
         {/* Avatar */}
         <div className="flex translate-y-2 flex-col gap-3">
           <div className="font-semibold text-blue-950">Avatar Default</div>
-          <div className="grid grid-cols-6 gap-2">
+          <RadioGroup
+            value={selectedAvatar}
+            onChange={setSelectedAvatar}
+            defaultValue={avatar1}
+            className="grid grid-cols-6 gap-1"
+          >
             {avatarOptions.map((avatar, index) => (
-              <div key={index}>
+              <Label
+                key={index}
+                className="rounded-full border-2 border-muted ring-offset-1 transition-transform duration-150 ease-in-out hover:scale-110  [&:has([data-state=checked])]:ring-2"
+              >
+                <RadioGroupItem value={avatar.src} className="sr-only" />
                 <Image
                   src={avatar.src}
                   alt={avatar.alt}
@@ -64,9 +87,9 @@ export default function EditPhotoProfile() {
                   height={50}
                   className="size-10 rounded-full"
                 />
-              </div>
+              </Label>
             ))}
-          </div>
+          </RadioGroup>
           <p className="text-sm text-slate-500">Select one for your profile </p>
           <div className="space-x-2 text-right">
             <Button variant="destructive" className="h-8 w-20">
