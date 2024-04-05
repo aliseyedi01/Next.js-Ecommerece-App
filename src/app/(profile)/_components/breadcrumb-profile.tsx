efb471990262f327eb65a6082eb5cf0cb2c56d2e
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-04-06 00:17:53 +0330

    ✨ feat(breadcrumb): dynamic based route

--- src/app/(profile)/_components/breadcrumb-profile.tsx
+++ src/app/(profile)/_components/breadcrumb-profile.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { SlashIcon } from "@radix-ui/react-icons";
-
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -11,11 +10,23 @@ import {
   BreadcrumbSeparator,
 } from "@components/ui";
 import { usePathname } from "next/navigation";
+import React from "react";
 
 export function BreadcrumbProfile() {
   const pathname = usePathname();
 
-  console.log("pathname", pathname);
+  // Split the pathname into segments
+  const segments = pathname.split("/").filter((segment) => segment !== "");
+
+  // Generate breadcrumb items dynamically
+  const breadcrumbItems = segments.map((segment, index) => {
+    const href = `/${segments.slice(0, index + 1).join("/")}`;
+    return (
+      <BreadcrumbItem key={segment}>
+        <BreadcrumbLink href={href}>{segment}</BreadcrumbLink>
+      </BreadcrumbItem>
+    );
+  });
 
   return (
     <Breadcrumb>
@@ -23,22 +34,14 @@ export function BreadcrumbProfile() {
         <BreadcrumbItem>
           <BreadcrumbLink href="/">Home</BreadcrumbLink>
         </BreadcrumbItem>
-
-        <BreadcrumbSeparator>
-          <SlashIcon />
-        </BreadcrumbSeparator>
-
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbSeparator>
-          <SlashIcon />
-        </BreadcrumbSeparator>
-
-        <BreadcrumbItem>
-          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-        </BreadcrumbItem>
+        {breadcrumbItems.map((item, index) => (
+          <React.Fragment key={index}>
+            <BreadcrumbSeparator>
+              <SlashIcon />
+            </BreadcrumbSeparator>
+            {item}
+          </React.Fragment>
+        ))}
       </BreadcrumbList>
     </Breadcrumb>
   );
