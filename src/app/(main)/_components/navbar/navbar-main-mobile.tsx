7764149e14240ed7b5591bb51838458a6fc3861f
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-04-05 22:37:09 +0330

    ✨ feat(navbar): update style for responsive

--- src/app/(main)/_components/navbar/navbar-main-mobile.tsx
+++ src/app/(main)/_components/navbar/navbar-main-mobile.tsx
@@ -16,9 +16,6 @@ import {
 } from "lucide-react";
 import { cn } from "@lib/utils";
 
-import Image from "next/image";
-import { navbarImage } from "@constants/images";
-
 const ItemsData = [
   {
     name: "Home",
@@ -51,37 +48,26 @@ export default function NavbarMainMobile() {
   const pathname = usePathname();
 
   return (
-    <div className="sticky bottom-3 z-40 mx-auto h-12 w-11/12  md:hidden">
-      <nav className="relative flex h-full items-center justify-between rounded-2xl bg-blue-500 px-10 md:flex">
+    <div className="sticky bottom-3 z-20 mx-auto h-12 w-11/12  md:hidden">
+      <nav className="flex h-full items-center justify-between  rounded-2xl bg-blue-500/50 px-4 backdrop-blur-2xl  md:flex">
         {ItemsData.map((item) => (
           <Link
             key={item.name}
             href={item.link}
             className={cn(
-              " flex flex-col items-center justify-center text-white transition-all duration-200 ease-in-out ",
-              pathname === item.link ? "bg-blue-800" : "",
+              " flex flex-col items-center justify-center gap-1   transition-all duration-500 ease-in-out",
+              pathname === item.link ? "" : "",
             )}
           >
             {React.cloneElement(item.icon as React.ReactElement, {
-              className: `size-4 absolute top-1 z-40 ${pathname === item.link ? "size-7  -translate-y-5" : ""}`,
+              className: `size-4 transition duration-200  ${pathname === item.link ? "size-6 rounded-full outline-[3px] outline-blue-700 outline-offset-8 text-blue-700 dark:text-yellow-500  dark:outline-yellow-400  " : ""}`,
             })}
 
-            <svg
-              className={cn(
-                " absolute -top-[30px] bottom-0 z-20 h-16 w-20  rotate-180  fill-background opacity-0",
-                pathname === item.link ? "opacity-100" : "",
-              )}
-              viewBox="0 0 144 62"
-              xmlns="http://www.w3.org/2000/svg"
+            <p
+              className={cn("text-xs ", pathname === item.link ? "hidden" : "")}
             >
-              <path
-                d="m111.34 23.88c-10.62-10.46-18.5-23.88-38.74-23.88h-1.2c-20.24 0-28.12 13.42-38.74 23.88-7.72 9.64-19.44 11.74-32.66 12.12v26h144v-26c-13.22-.38-24.94-2.48-32.66-12.12z"
-                fill="fill-red-200"
-                fill-rule="evenodd"
-              />
-            </svg>
-
-            <p className="absolute bottom-1 text-sm">{item.name}</p>
+              {item.name}
+            </p>
           </Link>
         ))}
       </nav>
