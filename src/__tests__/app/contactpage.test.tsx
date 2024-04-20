0175feb6dc764992b195911101793adeffa78566
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-04-20 21:48:41 +0330

    🧪 test(contactPage): add input element test

--- src/__tests__/app/contactpage.test.tsx
+++ src/__tests__/app/contactpage.test.tsx
@@ -4,9 +4,22 @@ import ContactPage from "@app/(main)/contact/_components/contact-page";
 import { render, screen } from "@testing-library/react";
 
 describe("Page", () => {
-  it("renders a contact page", () => {
+  // test text : Get In Touch
+  it("should have get in touch text", () => {
     render(<ContactPage />);
-
     expect(screen.getByText("Get In Touch")).toBeInTheDocument();
+  });
+
+  // test for button send
+  it("should have get in touch text", () => {
+    render(<ContactPage />);
+    expect(screen.getByRole("button", { name: "Send" })).toBeInTheDocument();
+  });
+
+  // test for username input
+  it("should render username input field", () => {
+    render(<ContactPage />);
+    const usernameInput = screen.getByPlaceholderText("Username");
+    expect(usernameInput).toBeInTheDocument();
   });
 });
